f35e53afd22a015fc79e30b68dd589d9df37d5e5
Nidafir <129302852+Nidafir@users.noreply.github.com>
2023-04-03 12:09:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 #this file consists of code for instances and sg
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIARSPNELGYCJEVYJ4K"
-secret_key = "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"
+access_key = "AKIASFD2GVVLOWWRC6MJ"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-0d81306eddc614a45"
+  ami             = "ami-06fdbb60c8e83aa5e"
   instance_type   = "t2.micro"
   key_name        = "rmk8s"
   vpc_security_group_ids = [aws_security_group.three.id]
@@ -25,7 +25,7 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-0d81306eddc614a45"
+  ami             = "ami-06fdbb60c8e83aa5e"
   instance_type   = "t2.micro"
   key_name        = "rmk8s"
   vpc_security_group_ids = [aws_security_group.three.id]
